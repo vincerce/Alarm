81a4f87a8f0061f16b8e6e387a5e62778a796f76
vince chao <358782833@qq.com>
2015-12-28 17:59:01 +0800

    删除多余第三方文件

---
+++ Alarm/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Alarm
+//
+//  Created by Superbro on 15/12/28.
+//  Copyright © 2015年 vince chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
